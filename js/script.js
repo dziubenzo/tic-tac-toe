61f7a6264c0f2aa0ffbc6470fc2bdbe0f7208b98
dziubenzo <dziubenzo@gmail.com>
2023-11-07 16:22:04 +0100

    Print board to the console as a formatted string

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 let gameBoard = (function () {
-  let boardArray = [
-    [, , ,],
-    [, , ,],
-    [, , ,],
+  // Create the game board as a 2D array
+  let board = [
+    ['e', 'e', 'e'],
+    ['e', 'e', 'e'],
+    ['e', 'e', 'e'],
   ];
-  return { boardArray };
+  // Print the formatted board to the console
+  let showBoard = function () {
+    console.log(`
+      ${board[0][0]}  |  ${board[0][1]}  |  ${board[0][2]}
+    -----|-----|-----
+      ${board[1][0]}  |  ${board[1][1]}  |  ${board[1][2]}
+    -----|-----|-----
+      ${board[2][0]}  |  ${board[2][1]}  |  ${board[2][2]}
+    `);
+  };
+  return { showBoard };
 })();
+
+gameBoard.showBoard();
